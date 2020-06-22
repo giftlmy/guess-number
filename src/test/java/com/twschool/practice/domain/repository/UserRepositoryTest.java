44aa057f0350c5efc27c11318f842a622d38a225
youyou <1094465220@qq.com>
2020-06-22 20:52:18 +0800

    modified user test

--- src/test/java/com/twschool/practice/domain/repository/UserRepositoryTest.java
+++ src/test/java/com/twschool/practice/domain/repository/UserRepositoryTest.java
@@ -13,14 +13,14 @@ public class UserRepositoryTest {
     @Test
     public void should_return_userInfo() {
         // given
-        User userInfo = new User("ys",new GuessNumberGame(new AnswerGenerator()),0,0);
+        User userInfo = new User("lmy",new GuessNumberGame(new AnswerGenerator()),0,0);
         UserRepository userRepository = new UserRepository();
 
         // when
-        userRepository.setUserInfo("ys",userInfo);
+        userRepository.setUserInfo("lmy",userInfo);
 
         //then
-        User result = userRepository.getUserInfoById("ys");
+        User result = userRepository.getUserInfoById("lmy");
         Assert.assertNotNull(result);
         Assert.assertEquals(0,result.getUserScore());
 
